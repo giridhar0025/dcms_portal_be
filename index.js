74fe0bd8009d193d0c05ca1be8c227757071a159
Giridhar <81301990+giridhar0025@users.noreply.github.com>
2025-06-09 15:16:08 +0530

    Serve Swagger docs using YAML

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
 const app = require('./src/config/express');
 const { connect: connectDB } = require('./src/config/database');
 const { connectQueue } = require('./src/config/queue');
+
+const swaggerDocument = YAML.load('./src/docs/api.yaml');
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
 
--- src/docs/swagger.js
+++
@@ -1,8 +0,0 @@
-const swaggerJSDoc = require('swagger-jsdoc');
-
-const options = {
-  definition: {},
-  apis: ['./src/docs/api.yaml'],
-};
-
-module.exports = swaggerJSDoc(options);
